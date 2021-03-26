febae3162acc36c173d6df486e2e9bce15c0199e
jemblonganvalley <jemblonganvalley@gmail.com>
2021-03-26 17:25:01 +0700

    update server to 9000

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 const index_loc = path.join(__dirname , '/public/client/build/index.html')
 const index = require('./routes/index_route.js')
 
+
 app.use(cors())
 app.use(express.static(path.join(__dirname , 'public/client/build/')))
 app.use(express.urlencoded({extended : false}))
@@ -15,4 +16,4 @@ app.use('/test', index)
 // console.log(bc.toString());
 
 
-app.listen(5000, ()=>{console.log('listen port 5000')})
+app.listen(9000, ()=>{console.log('listen port 5000')})
